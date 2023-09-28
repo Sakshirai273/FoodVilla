db0c8ba1856b3c03b75038b7939a9fa2367756b1
Sakshi <sakshirai273@gmail.com>
2023-09-28 20:54:57 +0530

    edit in app.js

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ import  ReactDOM from "react-dom/client";
 
 //Default import 
 import Header from "./components/Header";
-import Body from "./components/Header";
-import Footer from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 import { IMG_CDN_URL } from "./constants";
 
-const AppLayout = () =>{
+const AppLayout = () => {
   return (
-      <>
-          <Header/>
-          <Body/>
-          <Footer/>
-          </>
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Footer />
+    </React.Fragment>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
 
 //named import
 // import { Title } from "./components/Header";
