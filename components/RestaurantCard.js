1261616547d20d486caa666353e7f1516e63e47c
Sakshi <sakshirai273@gmail.com>
2023-09-28 13:45:39 +0530

    28sep

---
+++ components/RestaurantCard.js
@@ -0,0 +1,20 @@
+import { IMG_CDN_URL } from "../constants";
+
+// Restaurant card component: Image, name, cuisine
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  cuisines,
+  lastMileTravelString,
+}) => {
+  return (
+    <div className="card">
+      <img src={IMG_CDN_URL + cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString}</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
