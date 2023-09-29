ba934ec6fa6f3a3c1e9bcb8d595aadf230b1e09b
Sakshi <sakshirai273@gmail.com>
2023-09-29 18:18:26 +0530

    designed shimmer

--- components/shimmer.js
+++ components/shimmer.js
@@ -1,6 +1,14 @@
 const shimmer = () =>{
-    return <h1> shimmer UI Loading.....</h1>;
-
-};
+    // return <h1> shimmer UI Loading.....</h1>;
+    return(
+        <div className="restaurant-list">
+            {Array(10).fill("")
+            .map((e,index)=> (
+                <div key = {index} className="shimmer-card"></div>
+     )) }
+    
+        </div>
+        );
+    };
 
 export default shimmer;
