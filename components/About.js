ba17c4b988e18251bd71f546f6ee03afb8f70d22
Sakshi <sakshirai273@gmail.com>
2023-10-01 02:29:32 +0530

    about

---
+++ components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>ABOUT US PAGE</h1>
+      <p> This is namaste react course , finding path ch-7</p>
+    </div>
+  )
+}
+
+export default About;
