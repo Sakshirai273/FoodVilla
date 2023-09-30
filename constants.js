59bda8ff45852914067bbf80e36db130d41d514f
Sakshi <sakshirai273@gmail.com>
2023-10-01 02:29:49 +0530

    updated constant links

--- constants.js
+++ constants.js
@@ -6,6 +6,11 @@ export const IMG_CDN_URL =
 export const swiggy_api_URL =
 "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
+// Swiggy API to get Restaurant Menu data with corsproxy
+export const swiggy_menu_api_URL =
+"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+
+
 // RestaurantList is JSON Data for displaying cards
 // export const restaurantList = [
 //   {
