b7f47aad879864eb4e6e5bf6d562d29a96f5a99e
Sakshi <sakshirai273@gmail.com>
2023-10-28 20:04:14 +0530

    about.js before

--- components/About.js
+++ components/About.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Outlet} from "react-router-dom";
+import ProfileFunctionalComponent from './Profile';
+import Profile from './ProfileClass';
 
 const About = () => {
   return (
     <div>
       <h1>ABOUT US PAGE</h1>
-      <p> This is namaste react course , finding path ch-7</p>
+      <p> 
+        This is namaste react course , finding path ch-7
+        </p>
+        <ProfileFunctionalComponent name = {"Sakshi"} />
+        <Profile name = {"SakshiClass"} xyz = "abc"/>
     </div>
-  )
-}
+ );
+};
 
 export default About;
